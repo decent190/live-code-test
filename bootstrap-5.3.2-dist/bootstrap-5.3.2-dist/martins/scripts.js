e0007834c6c8bff2df3c80b67215d27c0f784067
samuel <samueloyelade6@gmail.com>
2024-02-02 11:00:10 +0100

    first commit

---
+++ bootstrap-5.3.2-dist/bootstrap-5.3.2-dist/martins/scripts.js
@@ -0,0 +1,19 @@
+// unary operator//
+// this is called oprand
+//e.g 5++
+
+
+//binary operator//
+//two oprands
+// e.g  
+// 2 + 2 -,+,/
+
+// ternary operator
+//three operands
+// e.g 
+// if (2+2 == 4) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+// 2+2==4?console.log("Yes"):console.log("no");
